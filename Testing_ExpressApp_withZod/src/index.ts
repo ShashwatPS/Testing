3612d2a3ec08dbfd6909c6dac50704abba219e9b
Shashwat Singh <shashwat123student@gmail.com>
2024-06-25 07:59:01 +0530

    Done testing with zod

---
+++ Testing_ExpressApp_withZod/src/index.ts
@@ -0,0 +1,31 @@
+import express =  require('express');
+import { z } from 'zod';
+import request =  require("supertest");
+export const app = express();
+app.use(express.json());
+
+const sumSchema = z.object({
+    a: z.number(),
+    b: z.number(),
+});
+
+app.post('/sum', (req, res) => {
+    const parsedResponse = sumSchema.safeParse(req.body);
+    if (!parsedResponse.success) {
+        res.status(411).json({ message: "Incorrect Inputs"});
+    }
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+    res.status(200).json({ result: answer });
+});
+
+app.get('/sum',(req,res)=>{
+    const pparsedResponse = sumSchema.safeParse({
+        a: Number(req.headers["a"]),
+        b: Number(req.headers["b"])
+    });
+    if (!pparsedResponse.success) {
+        res.status(411).json({ message: "Incorrect Inputs"});
+    }
+    const answer = pparsedResponse.data.a + pparsedResponse.data.b;
+    res.status(200).json({ result: answer });
+})
